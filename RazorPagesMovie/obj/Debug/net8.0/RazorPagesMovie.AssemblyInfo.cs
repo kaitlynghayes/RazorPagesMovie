a6f85385518cbca105ddb9214fa2d501b0f834bb
Kaitlyn Hayes <hayeskaitlyn10@gmail.com>
2024-07-12 15:36:41 -0500

    adding git ignore

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56020f8e9d6217326596fc0fab0ee63ee311d55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6331a25fc298da72e33710d156104f0527c9da")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
